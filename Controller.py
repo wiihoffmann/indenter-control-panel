bf29172ed072f377746ed5dc0801dd62a0458621
Raamish N <59901620+raamish10@users.noreply.github.com>
2022-02-22 14:47:04 -0700

    updates

--- Controller.py
+++ Controller.py
@@ -1,4 +1,5 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+# import gpiozero as GPIO  # Import Raspberry Pi GPIO library
 import threading
 import time
 
@@ -19,46 +20,42 @@ def moveAsync(direction):
         GPIO.setup(directionPin, GPIO.OUT, initial=GPIO.LOW)
 
     while moving == True:
-        GPIO.output(stepPin, GPIO.HIGH) # Turn on
+        GPIO.output(stepPin, GPIO.HIGH)  # Turn on
         time.sleep(pulseDelay)
-        GPIO.output(stepPin, GPIO.LOW) # Turn off
+        GPIO.output(stepPin, GPIO.LOW)  # Turn off
         time.sleep(pulseDelay)
 
 
 class Controller():
-    
+
     def __init__(self, stepPinNumber, directionPinNumber):
         global stepPin, directionPin
         stepPin = stepPinNumber
         directionPin = directionPinNumber
-        
-        GPIO.setwarnings(False) # Ignore warning for now
-        GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-        GPIO.setup(stepPin, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+
+        GPIO.setwarnings(False)  # Ignore warning for now
+        GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+        # Set pin 8 to be an output pin and set initial value to low (off)
+        GPIO.setup(stepPin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(directionPin, GPIO.OUT, initial=GPIO.LOW)
 
-
-
-    def startMovingDown(self, stepFreq = DEFAULT_STEP_RATE):
+    def startMovingDown(self, stepFreq=DEFAULT_STEP_RATE):
         global moving, stepRate
         moving = True
         stepRate = stepFreq
         # direction 1 for down
         threading.Thread(target=moveAsync, args=(1,)).start()
 
-
     def stopMovingDown(self):
         global moving
         moving = False
 
-
-    def startMovingUp(self, stepFreq = DEFAULT_STEP_RATE):
+    def startMovingUp(self, stepFreq=DEFAULT_STEP_RATE):
         global moving, stepRate
         moving = True
         stepRate = stepFreq
         # direction 0 for up
         threading.Thread(target=moveAsync, args=(0,)).start()
-
 
     def stopMovingUp(self):
         global moving
--- MainUI.py
+++ MainUI.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import*
 from PyQt5.uic import loadUi
 
-from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.backends.backend_qt5agg import (
+    NavigationToolbar2QT as NavigationToolbar)
 
 import numpy as np
 import random
@@ -9,86 +10,106 @@ import random
 
 from Controller import *
 
+
 class MainWindow(QMainWindow):
-    
+
     def __init__(self, UIFileName):
         # initialize Qt for the GUI
         QMainWindow.__init__(self)
-        loadUi(UIFileName,self)
+        loadUi(UIFileName, self)
         self.setWindowTitle("Indenter Control Panel")
         # set up bindings for the buttons and widgets
-        self.pushButton_generate_random_signal.clicked.connect(self.updateGraph)
+        self.pushButton_generate_random_signal.clicked.connect(
+            self.updateGraph)
         self.pushButton_clear_graph.clicked.connect(self.clearGraph)
         self.LoadButton.clicked.connect(self.loadFile)
         self.SaveButton.clicked.connect(self.saveFile)
         self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
-        self.spinBox.valueChanged.connect(self.show_result)
-        self.setButton.clicked.connect(self.display_force)
-        
+        self.forceText.insertPlainText("10")
+
+        self.incrementButton.setAutoRepeat(True)
+        self.decrementButton.setAutoRepeat(True)
+
+        self.incrementButton.pressed.connect(
+            lambda: self.update_force('increment'))
+        self.decrementButton.pressed.connect(
+            lambda: self.update_force('decrement'))
+
+        self.startButton.clicked.connect(self.startProgram)
+        self.stopButton.clicked.connect(self.stopProgram)
+
         self.upButton.pressed.connect(self.startMovingUp)
         self.upButton.released.connect(self.stopMovingUp)
-        
+
         self.downButton.pressed.connect(self.startMovingDown)
         self.downButton.released.connect(self.stopMovingDown)
 
         # set up the motor and ADC controller
-        self.controller = Controller(12,16)
-
+        self.controller = Controller(12, 16)
 
     def updateGraph(self):
         f = random.randint(1, 50)
         length_of_signal = 10000
-        t = np.linspace(0,1,length_of_signal)
-        
+        t = np.linspace(0, 1, length_of_signal)
+
         cosinus_signal = np.cos(2*np.pi*f*t)
         sinus_signal = np.sin(2*np.pi*f*t)
 
         self.MplWidget.canvas.axes.clear()
         self.MplWidget.canvas.axes.plot(t, cosinus_signal)
         self.MplWidget.canvas.axes.plot(t, sinus_signal)
-        self.MplWidget.canvas.axes.legend(('cosine', 'sine'),loc='upper right')
+        self.MplWidget.canvas.axes.legend(
+            ('cosine', 'sine'), loc='upper right')
         self.MplWidget.canvas.axes.set_title('Cosine - Sine Signal')
         self.MplWidget.canvas.figure.tight_layout()
         self.MplWidget.canvas.draw()
-        
 
     def clearGraph(self):
         self.MplWidget.canvas.axes.clear()
-        self.MplWidget.canvas.axes.legend(('cosine', 'sine'),loc='upper right')
+        self.MplWidget.canvas.axes.legend(
+            ('cosine', 'sine'), loc='upper right')
         self.MplWidget.canvas.axes.set_title('Cosine - Sine Signal')
         self.MplWidget.canvas.draw()
-        self.forceTextEdit.setText('Cleared graph')
-
 
     def saveFile(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","CSV File (*.csv)", options=options)
+        fileName, _ = QFileDialog.getSaveFileName(
+            self, "QFileDialog.getSaveFileName()", "", "CSV File (*.csv)", options=options)
         if fileName:
             print(fileName)
-
 
     def loadFile(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;CSV Files (*.csv)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(
+            self, "QFileDialog.getOpenFileName()", "", "All Files (*);;CSV Files (*.csv)", options=options)
         if fileName:
             print(fileName)
 
-    def show_result(self):
-        value = self.spinBox.value()
-        
-    def display_force(self):
-        self.forceTextEdit.setText('Applying force of: ' + str(self.spinBox.value()) + ' N')
-    
+    def update_force(self, _str):
+        init_force = int(self.forceText.toPlainText())
+        new_force = (init_force+5) if _str == 'increment' else (init_force-5)
+
+        if(0 <= new_force <= 100):
+            self.forceText.setText(str(new_force))
+        else:
+            self.forceText.setText('0')
+
     def startMovingUp(self):
         self.controller.startMovingUp(1000)
 
     def stopMovingUp(self):
-        self.controller.stopMovingUp() 
+        self.controller.stopMovingUp()
 
     def startMovingDown(self):
         self.controller.startMovingDown(1000)
 
     def stopMovingDown(self):
         self.controller.stopMovingDown()
+
+    def startProgram(self):
+        print("started")
+
+    def stopProgram(self):
+        print("stopped")
