eb6f5bfbd624ab847311e9f90ab6a40878dc0853
wiihoffmann <wiihoffmann@yahoo.ca>
2022-05-09 11:29:02 -0600

    adjust defaults

--- Config.py
+++ Config.py
@@ -3,14 +3,14 @@
 
 ######## preload settings #########
 DEFAULT_PRELOAD_TIME = 1        # seconds
-DEFAULT_PRELOAD = 5             # newtons
+DEFAULT_PRELOAD = 8             # newtons
 MIN_PRELOAD = 5                 # newtons
 MAX_PRELOAD = 30                # newtons
 PRELOAD_INCREMENT_SIZE = 1      # newtons
 
 ######## full load settings #######
 DEFAULT_MAX_LOAD_TIME = 2       # seconds
-DEFAULT_MAX_LOAD = 100           # newtons
+DEFAULT_MAX_LOAD = 100          # newtons
 MIN_LOAD = 5                    # newtons
 MAX_LOAD = 110                  # newtons
 MAX_LOAD_INCREMENT_SIZE = 5     # newtons
@@ -21,11 +21,11 @@ MAX_HOLD_TIME = 15              # seconds
 HOLD_TIME_INCREMENT_SIZE = 1    # seconds
 
 ######## step rate settings #######
-EMERGENCY_STOP_STEP_RATE = 2100 # steps/second (How fast to retract indenter during an emergency stop.)
-JOG_SPEED = 1000                # steps/second (Manual move up/down speed.)
-DEFAULT_STEP_RATE = 1000        # steps/second (Default rate used when taking a measurement.)
-MIN_STEP_RATE = 500            # steps/second
-MAX_STEP_RATE = 2500            # steps/second
+EMERGENCY_STOP_STEP_RATE = 2000 # steps/second (How fast to retract indenter during an emergency stop.)
+JOG_SPEED = 600                 # steps/second (Manual move up/down speed.)
+DEFAULT_STEP_RATE = 600         # steps/second (Default rate used when taking a measurement.)
+MIN_STEP_RATE = 200             # steps/second
+MAX_STEP_RATE = 1500            # steps/second
 STEP_RATE_INCREMENT_SIZE = 100  # steps/second
 
 ######### misc settings ###########
