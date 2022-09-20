7618e84fdd3069da28c4ce6c4cd55a88e1500e1b
wiihoffmann <Wiihoffmann@yahoo.ca>
2022-09-20 14:14:50 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,18 +6,13 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Enable hardware PWM
-echo "dtoverlay=pwm-2chan" >> /boot/config.txt
-# Enable I2C
-raspi-config nonint do_i2c 0
-
 # update software so that we pull the latest packages
 apt update -y
 apt full-upgrade -y
 
 # install dependencies 
 apt install git xvkbd python3 python3-pip python3-pyqt5 python3-numpy -y
-pip3 install rpi-hardware-pwm pyqtgraph Adafruit-Blinka adafruit-circuitpython-ads1x15
+pip3 install pyqtgraph
 
 # get the code from the repo
 git clone https://github.com/ECE-492-capstone/spinal-stiffness-indenter
