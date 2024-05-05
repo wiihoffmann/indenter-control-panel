85971cc83d0943aeb370e25092c6802dbc1e01d6
wiihoffmann <Wiihoffmann@yahoo.ca>
2024-05-05 04:23:42 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # make sure script is run as root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root! Try \"sudo ./setup.sh\"" 
-   exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#   echo "This script must be run as root! Try \"sudo ./setup.sh\"" 
+#   exit 1
+#fi
 
 # update software so that we pull the latest packages
-apt update -y
-apt full-upgrade -y
+sudo apt update -y
+sudo apt full-upgrade -y
 
 # install dependencies 
-apt install git onboard python3 python3-pip python3-pyqt5 python3-numpy python3-pyqtgraph -y
+sudo apt install git onboard python3 python3-pip python3-pyqt5 python3-numpy python3-pyqtgraph -y
 
 # auto launch onboard
 mkdir ~/.config/autostart
